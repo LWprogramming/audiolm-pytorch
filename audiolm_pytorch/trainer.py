a43fe447e890b3cb1a03bcc12bbf60b3b559dad9
Leon Wu <lwgithub@gmail.com>
2023-07-17 18:33:16 -0700

    dump accelerator info

--- audiolm_pytorch/trainer.py
+++ audiolm_pytorch/trainer.py
@@ -988,7 +988,7 @@ class CoarseTransformerTrainer(nn.Module):
         # update vae (generator)
 
         # print(f"coarse training on device {device}")
-        self.print(f"dumping accelerator rng_types and synchronized_generator and self.dl's device (i.e. is dataloader on cpu or gpu?): found {self.accelerator.rng_types} and {self.accelerator.synchronized_generator} and {self.dl.device}")
+        self.print(f"dumping accelerator rng_types and synchronized_generator and self.dl's device (i.e. is dataloader on cpu or gpu?): found {self.accelerator._dataloaders[0].rng_types} and {self.accelerator._dataloaders[0].synchronized_generator} and {self.dl.device}. accelerator has {len(self.accelerator._dataloaders)} dataloaders")
         for _ in range(self.grad_accum_every):
             data_kwargs = dict(zip(self.ds_fields, next(self.dl_iter)))
             if self.steps == 0 and _ == 0:
