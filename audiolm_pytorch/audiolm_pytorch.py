c0447af00c29481ec3e916cd04102b277bc9f0ef
Leon Wu <lwgithub@gmail.com>
2023-07-24 18:01:06 -0700

    ?

--- audiolm_pytorch/audiolm_pytorch.py
+++ audiolm_pytorch/audiolm_pytorch.py
@@ -507,10 +507,12 @@ class SemanticTransformer(nn.Module):
         assert path.exists()
         print(f"device for semantic transformer: {device}")
         pkg = torch.load(str(path), map_location = device)
+        print(f"pkg device: {pkg['model'].device}")
         # check version
         if 'version' in pkg and version.parse(pkg['version']) < version.parse(__version__):
             print(f'model was trained on older version {pkg["version"]} of audiolm-pytorch')
         self.load_state_dict(pkg['model'])
+        print(f"package device and self deivce {pkg['model'].device} {self.device}")
         return pkg
 
     def forward_with_cond_scale(
